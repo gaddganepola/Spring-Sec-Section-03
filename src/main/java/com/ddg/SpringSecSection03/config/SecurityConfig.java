a621f316131428c10525ce8ebb01922dddb7dd61
gaddganepola <ganeypola@gmail.com>
2025-11-23 17:30:38 +0530

    create in memory users

--- src/main/java/com/ddg/SpringSecSection03/config/SecurityConfig.java
+++ src/main/java/com/ddg/SpringSecSection03/config/SecurityConfig.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -21,6 +25,14 @@ public class SecurityConfig {
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
+    }
+
+//    create in memory users
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("user").password("{noop}0000").authorities("read").build();
+        UserDetails admin = User.withUsername("admin").password("{noop}1111").authorities("admin").build();
+        return new InMemoryUserDetailsManager(user, admin);
     }
 
 }
